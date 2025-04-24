8a47c8e1aec2baf432caab27cf5c9e5e837df02b
Syaiful313 <mthitz313@gmail.com>
2025-04-24 12:39:37 +0700

    feat: implement authentication store and update login functionality

--- src/hooks/api/auth/useLogin.ts
+++ src/hooks/api/auth/useLogin.ts
@@ -1,6 +1,7 @@
 "use client";
 
 import { axiosInstance } from "@/lib/axios";
+import { useAuthStore } from "@/stores/auth";
 import { User } from "@/types/user";
 import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
@@ -8,15 +9,20 @@ import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 const useLogin = () => {
-    const router = useRouter()
+  const router = useRouter();
+  const { onAuthSuccess } = useAuthStore();
   return useMutation({
     mutationFn: async (payload: Pick<User, "email" | "password">) => {
       const { data } = await axiosInstance.post("/auth/login", payload);
       return data;
     },
-    onSuccess: () => {
+    onSuccess: (data) => {
       toast.success("Login successfully");
-      router.push("/")
+      onAuthSuccess({
+        user: data,
+        accessToken: data.accessToken,
+      });
+      router.push("/");
     },
     onError: (error: AxiosError<any>) => {
       toast.error(error.response?.data.message);
